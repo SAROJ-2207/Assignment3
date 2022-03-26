b0d568b59057d5e364858b7d27442c62293fc3e7
SAROJ-2207 <85055860+SAROJ-2207@users.noreply.github.com>
2022-03-26 15:48:26 +0530

    Add files via upload

---
+++ command.sh
@@ -0,0 +1,28 @@
+$pwd   :- Present working Directory
+
+$touch :- create a file
+
+$ls    :- display file
+
+$clear :- Clear the screen
+
+$mkdir :- Create a Directory
+
+$cd ../:- Move to previous Directory
+
+$cd .  :- Same directory / Present Directory
+
+$ls .. :- Access all file previous directory
+
+$ls -a :- It shows all files with hidden files
+
+$cd ~  :- root directory
+
+$cp    :- Copy file
+
+$rm -r :- This command not check folder is empty or not directly deleted
+
+$rmdir :- Delete only empty folder
+
+$grep  :- It will show releted data
+
